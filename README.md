726d0a1ca52d6cc3ba7f06a1d5a426c939d91799
MsftKings <ryan.h.king@outlook.com>
2025-02-23 11:23:09 -0500

    item add to list

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Minimize-Food-Waste-Application
+
 Recently, I noticed that I spend a lot of time cleaning out a fridge of dinners from the past. What if there was an app that you could enter in what you bought and be given meals that would use those ingredients and create minimal food waste? Well... let's try to make one.
---
+++ script.js
@@ -0,0 +1,36 @@
+let inventory = [];
+
+function addItem() {
+    const itemName = document.getElementById("itemName").value;
+    const itemQuantity = parseInt(document.getElementById("inputQuantity").value);
+
+    if (itemName && itemQuantity > 0) {
+        const item = { name: itemName, quantity: itemQuantity };
+        inventory.push(item);
+        displayInventory();
+    } else {
+        alert("Please enter a valid item name and quantity");
+    }
+}
+
+function removeItem() {
+    const itemName = document.getElementById("itemName").value;
+
+    if (itemName) {
+        inventory = inventory.filter(item => item.name !== itemName);
+        displayInventory();
+    } else {
+        alert("Please enter a valid item name to remove");
+    }
+}
+
+function displayInventory() {
+    const inventoryList = document.querySelector("#pantryInventory ul");
+    inventoryList.innerHTML = '';
+
+    inventory.forEach(item => {
+        const listItem = document.createElement('li');
+        listItem.textContent = `${item.name}: ${item.quantity}`;
+        inventoryList.appendChild(listItem);
+    });
+}
