bd354c4a5cc9133c86a0a1088d44c7033a786aeb
MsftKings <ryan.h.king@outlook.com>
2025-02-25 14:26:56 -0500

    Adjustments to the Hero for mobile view

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const menuOpenButton = document.querySelector("#menuOpenButton");
+const menuCloseButton = document.querySelector("#menuCloseButton");
+
 
 
     //Toggle Mobile menu
 
-menuOpenButton.addEventListener("onClick", () => {
+menuOpenButton.addEventListener("click", () => {
+    document.body.classList.toggle("showMobileMenu")
+});
+
+menuCloseButton.addEventListener("click", () => {
     document.body.classList.toggle("showMobileMenu")
 });
